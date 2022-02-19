ed218f51b6311fcad7ca38b64b1440aad2d76a0a
Rafael GPL <rafaellopez@reps2.com>
2022-02-19 11:59:59 -0800

    First commit

---
+++ posts/views.py
@@ -0,0 +1,41 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from django.urls import reverse_lazy
+
+
+class PostListView(ListView):
+    template_name = "post_list.html"
+    model = Post
+
+
+class PostDetailView(DetailView):
+    template_name = "post_detail.html"
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "post_new.html"
+    model = Post
+    fields = ["title", "author", "body"]
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "post_edit.html"
+    model = Post
+    fields = ["title", "body"]
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "post_delete.html"
+    model = Post
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
